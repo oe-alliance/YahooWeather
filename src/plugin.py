afa22a49ed7279b22263427e7c805d29aae023c4
rossi2000 <rossi_2000@hotmail.com>
2015-06-17 23:35:20 +0100

    add Full-HD logo and add fallback to sd logo.

--- src/plugin.py
+++ src/plugin.py
@@ -2,11 +2,16 @@ from . import _
 from Plugins.Plugin import PluginDescriptor
 from Weather import *
 from Search_Id import *
+from enigma import getDesktop
 
 def main(session,**kwargs):
 	session.open(MeteoMain)
 
 def Plugins(**kwargs):
-	list = [PluginDescriptor(name=_("Yahoo Weather"), description=_("5 Day Weather Forecast"), where = [PluginDescriptor.WHERE_PLUGINMENU, PluginDescriptor.WHERE_EXTENSIONSMENU], icon = "plugin.png", fnc=main)]
+	screenwidth = getDesktop(0).size().width()
+	if screenwidth and screenwidth == 1920:
+		list = [PluginDescriptor(name=_("Yahoo Weather"), description=_("5 Day Weather Forecast"), where = [PluginDescriptor.WHERE_PLUGINMENU, PluginDescriptor.WHERE_EXTENSIONSMENU], icon = "pluginhd.png", fnc=main)]
+	else:
+		list = [PluginDescriptor(name=_("Yahoo Weather"), description=_("5 Day Weather Forecast"), where = [PluginDescriptor.WHERE_PLUGINMENU, PluginDescriptor.WHERE_EXTENSIONSMENU], icon = "plugin.png", fnc=main)]
 	return list
 
